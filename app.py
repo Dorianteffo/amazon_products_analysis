9e22154cb6392e97fc8b52e043c42ebe648fbf09
Dorian <dorianteffo@gmail.com>
2023-07-25 19:01:46 +0100

    Two new plots on the website

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ def cleaning_data(df) :
     df.loc[df['actual_price'].str.contains(',')==True,'actual_price'] = df.loc[df['actual_price'].str.contains(',')==True,'actual_price'].apply(lambda x:x.replace(',',''))
     df['actual_price'] = df['actual_price'].astype('float')
 
+    df['discount_price'] = df['discount_price']*0.012
+    df['actual_price'] = df['actual_price']*0.012
+
     df.insert(1, 'manufacturer', df['name'].apply(lambda x:x.split(' ')[0]))
 
     df['discount_percentage'] = np.where(df['actual_price']!=0,round((df['actual_price']-df['discount_price'])/df['actual_price'],2),0)
@@ -52,13 +55,60 @@ df = cleaning_data(df)
 
 
 #sidebar 
-st.sidebar.write('## Filter')
-manufactur = st.sidebar.multiselect("Select a manufacturer:", 
-                                    options = df['manufacturer'].unique())
+st.sidebar.write('# Filter')
+manufactur = st.sidebar.selectbox("Select a manufacturer:", 
+                                    options = df['manufacturer'].unique()
+                                    )
+
+
+
+st.sidebar.write('You selected:', manufactur)
 
 df_selection = df.query(
     " manufacturer == @manufactur"
 )
 
 
-st.dataframe(df_selection)
+total_revenue = round(df_selection['discount_price'].sum(),0)
+avg_ratings = round(df_selection['ratings'].mean(),1)
+star_rating = '⭐'* int(round(avg_ratings,0))
+avg_discount = round(df_selection['discount_percentage'].mean()*100,2)
+
+col1, col2, col3  = st.columns(3)
+with col1 : 
+    st.subheader('Total Sales:')
+    st.subheader(f'US $ {total_revenue:,}')
+with col2 : 
+    st.subheader('Average Rating:')
+    st.subheader(f'{avg_ratings} {star_rating} ')
+with col3 : 
+    st.subheader('Average Discount:')
+    st.subheader(f'{avg_discount} %')
+
+
+st.markdown('---')
+
+sales_by_category = df_selection.groupby('main_category',as_index=False)['discount_price'].sum()
+fig_category_sales = px.bar(
+    sales_by_category, x='main_category',
+      y='discount_price', 
+      template='simple_white'
+      )
+fig_category_sales.update_layout(title = "Sales by category",
+                                 xaxis_title = "Category",
+                                 yaxis_title = "Sales (US $)")
+
+st.plotly_chart(fig_category_sales)
+
+fig_discount_reviews = px.scatter(df_selection, 
+                                  x="discount_percentage", 
+                                  y="no_of_ratings", 
+                                  trendline="ols", 
+                                   template="simple_white"
+                                   )
+fig_discount_reviews.update_yaxes(range=[0, 8000])
+fig_discount_reviews.update_layout(title = "Relationship between the number of reviews and discount percent",
+                 yaxis_title = "Number of reviews",
+                 xaxis_title = "Discount percentage")
+
+st.plotly_chart(fig_discount_reviews)
